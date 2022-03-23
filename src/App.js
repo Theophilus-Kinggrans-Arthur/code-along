e7aba5eb7a240e80c2eebf82bdcd7cd5104d8a36
Theophilus Kinggrans Arthur <Becewassceking22@gmail.com>
2022-03-23 14:06:36 +0000

    react-components-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import writers from "./writers";
+import { useEffect, useState } from "react";
+// import writers from "./writers";
 import { ProfileCard } from "./ProfileCard";
 
 function App() {
+  const [writers, setWriters] = useState([]);
+
+  useEffect(() => {
+    const getWriters = async () => {
+      const response = await fetch("/writers.json");
+      const data = await response.json();
+      console.log(data);
+      setWriters(data);
+    };
+    getWriters();
+  }, []);
+
   return (
     <div>
-      <h1>writers profile</h1>
+      <h1>Writers Profile</h1>
       <div className="container">
         {writers.map((writer) => (
           <ProfileCard writer={writer} />
